aa95ee8fba18495e6495277bb7826dc9af369884
Tomoharu Watanabe <tomoharu.dev@gmail.com>
2024-04-25 21:11:26 +0900

    サンプルでsampe-usersの読み取りを実装

--- backend/src/main.py
+++ backend/src/main.py
@@ -17,3 +17,13 @@ app = FastAPI()
 @app.get("/", response_class=PlainTextResponse)
 def read_root():
     return "技育CAMP2024ハッカソンVol.4 Topaz Taamagawa (Backend)"
+
+@app.get("/users")
+async def get_users():
+    users_stream = firestore.collection("sample-users").stream()
+
+    users = {}
+    async for user_snpashot in users_stream:
+        users[user_snpashot.id] = user_snpashot.to_dict()
+
+    return users
