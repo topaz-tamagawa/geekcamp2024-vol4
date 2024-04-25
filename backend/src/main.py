b8bb8aef4e04d2bb84a9e52001f414dce04890d7
Tomoharu Watanabe <tomoharu.dev@gmail.com>
2024-04-25 21:10:53 +0900

    firebaseとfirestoreのセットアップコードを追加

--- backend/src/main.py
+++ backend/src/main.py
@@ -6,6 +6,12 @@ import os
 from dotenv import load_dotenv
 load_dotenv(verbose=True)
 
+from .lib.firebase import init_firebase_app
+init_firebase_app()
+
+from firebase_admin import firestore_async
+firestore = firestore_async.client()
+
 app = FastAPI()
 
 @app.get("/", response_class=PlainTextResponse)
