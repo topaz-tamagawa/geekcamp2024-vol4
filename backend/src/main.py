993a3b4f7b9e1cca0057f649c4a92edd3a31c133
Tomoharu Watanabe <tomoharu.dev@gmail.com>
2024-04-26 12:55:56 +0900

    usersのサンプルを追加

--- backend/src/main.py
+++ backend/src/main.py
@@ -12,6 +12,8 @@ init_firebase_app()
 from firebase_admin import firestore_async
 firestore = firestore_async.client()
 
+from pydantic import BaseModel
+
 app = FastAPI()
 
 @app.get("/", response_class=PlainTextResponse)
@@ -27,3 +29,13 @@ async def get_users():
         users[user_snpashot.id] = user_snpashot.to_dict()
 
     return users
+
+class SampleUser(BaseModel):
+    name: str
+
+@app.post("/users")
+async def post_users(user: SampleUser, user_id: str):
+    users_ref = firestore.collection("sample-users")
+    await users_ref.document(user_id).set(user.model_dump())
+    return user_id
+    
