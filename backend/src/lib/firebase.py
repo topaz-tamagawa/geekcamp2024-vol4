4d3f5906641b580c9fccaa69afefb15b6803d23c
Tomoharu Watanabe <tomoharu.dev@gmail.com>
2024-04-27 05:17:58 +0900

    firebase initをcloud runに対応

--- backend/src/lib/firebase.py
+++ backend/src/lib/firebase.py
@@ -5,8 +5,12 @@ import firebase_admin
 import json
 
 def init_firebase_app():
-    credentials_base64 = os.environ["FIREBASE_SERVICE_ACCOUNT_CREDENTIALS"]
-    credentials_json = json.loads(base64.b64decode(credentials_base64).decode("utf-8"))
-    certificate = firebase_admin.credentials.Certificate(credentials_json)
-    app = firebase_admin.initialize_app(certificate)
+    if "FIREBASE_SERVICE_ACCOUNT_CREDENTIALS" in os.environ :
+        credentials_base64 = os.environ["FIREBASE_SERVICE_ACCOUNT_CREDENTIALS"]
+        credentials_json = json.loads(base64.b64decode(credentials_base64).decode("utf-8"))
+        certificate = firebase_admin.credentials.Certificate(credentials_json)
+        app = firebase_admin.initialize_app(certificate)
+    else:
+        app = firebase_admin.initialize_app()
+
     return app
